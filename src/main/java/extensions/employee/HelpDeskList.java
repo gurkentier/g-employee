6960edd467b8652b7d495a790ce8d6a785bc6a42
gurkentier <d.schmit@m3connect.de>
2020-08-09 18:25:23 +0200

    add HelpDeskList (WIP)

---
+++ src/main/java/extensions/employee/HelpDeskList.java
@@ -0,0 +1,38 @@
+package extensions.employee;
+
+
+import gearth.extensions.parsers.HPoint;
+
+import java.util.ArrayList;
+
+public class HelpDeskList {
+
+    private ArrayList<HelpDesk> helpDesks = new ArrayList<>();
+
+    public HelpDesk getDeskByOwnerPoint(HPoint point) {
+        HelpDesk result = null;
+        for (HelpDesk pair : helpDesks) {
+            HPoint ownerPoint = pair.owner.point;
+            assert ownerPoint != null;
+            if (ownerPoint.getX() == point.getX() && ownerPoint.getY() == point.getY()) {
+                result = pair;
+            }
+        }
+
+        return result;
+    }
+
+    public HelpDesk getDeskByTenantPoint(HPoint point) {
+        HelpDesk result = null;
+        for (HelpDesk pair : helpDesks) {
+            HPoint tenantPoint = pair.tenant.point;
+            assert tenantPoint != null;
+            if (tenantPoint.getX() == point.getX() && tenantPoint.getY() == point.getY()) {
+                result = pair;
+            }
+        }
+
+        return result;
+    }
+
+}
